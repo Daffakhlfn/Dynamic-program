15c85e37f07f36cccb3968f4d42fadc9d6f5c93e
Asus <Asus@DESKTOP-8UAA3ST>
2018-09-10 11:09:17 +0700

    Initial Commit

---
+++ src/HitungFibonacci/Praktek1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HitungFibonacci;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+/**
+ *
+ * @author Asus
+ */
+public class Praktek1 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String[] args) {
+        String identitas = "Mohammad Daffa Khalfani Sujatmiko / X RPL 3 / 23";
+        
+        tampilJudul(identitas);
+    }
+}
