f80c53929c040b20af701ee315fab22e9c5d1d9f
Asus <Asus@DESKTOP-8UAA3ST>
2018-09-12 13:03:31 +0700

    Tambah Tampil Hasil

--- src/HitungFibonacci/Praktek1.java
+++ src/HitungFibonacci/Praktek1.java
@@ -25,6 +25,7 @@ public class Praktek1 {
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
     private static int tampilInput()
     {
@@ -49,5 +50,9 @@ public class Praktek1 {
         }
         
         return hasil[n-1];
+    }
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
     }
 }
