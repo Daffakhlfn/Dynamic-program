af9fb5334476261307ef89b00eadbecde282454a
Asus <Asus@DESKTOP-8UAA3ST>
2018-09-14 18:59:12 +0700

    Praktek1

---
+++ src/HitungFibonacci/Tugas02.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HitungFibonacci;
+import java.util.*;
+/**
+ *
+ * @author Asus
+ */
+public class Tugas02 {
+    public static void main(String[] args)
+    {
+        int[] Berat = new int[]{7, 5, 4, 3, 2};
+        int[] Harga = new int[]{10000, 7000, 5000, 3000, 2000};
+        int berat = Input();
+        
+        Proses(berat, Berat, Harga);
+    }
+    private static int Input()
+    {
+        Scanner mbois = new Scanner(System.in);
+        
+        System.out.print("Berat Barang : ");
+        int berat = mbois.nextInt();
+        
+        return berat;
+    }
+    private static void Proses(int berat, int[] Berat, int[] Harga)
+    {
+        int jumlah = 0;
+        for(int i = 0; i < Berat.length; i++)
+        {
+            int n = 0;
+            while(berat >= Berat[i])
+            {
+                berat = berat - Berat[i];
+                n++;
+            }
+            int total = n*Harga[i];
+            System.out.println(Berat[i] + " Sebanyak " + n + " dengan harga Rp " + total);
+            jumlah = jumlah + total;
+        }
+        System.out.println("\nTotal Bayaran : Rp" + jumlah);
+    }
+}
